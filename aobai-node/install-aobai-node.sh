ee3d2b1e8317249de2016a2ee170893f9a4d568e
jeff <jeff@infowis.com>
2026-07-26 12:45:49 +0700

    publish speedtest frontend bundle

--- aobai-node/install-aobai-node.sh
+++ aobai-node/install-aobai-node.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 REPO_RAW="${AOBAI_REPO_RAW:-https://raw.githubusercontent.com/5UFKEFU/XrayR-release/master/aobai-node}"
-ARCHIVE_REVISION="48b33a0"
+ARCHIVE_REVISION="aec9e0a"
 ARCHIVE_URL="${AOBAI_ARCHIVE_URL:-https://raw.githubusercontent.com/5UFKEFU/XrayR-release/$ARCHIVE_REVISION/aobai-node/aobai-node-linux-amd64.tar.gz}"
 PANEL_URL="${PANEL_URL:-https://www.5ufkefu.com}"
 MU_KEY="${MU_KEY:-5uf5uf}"
